b79bb67b0f509b4941dabf3ccd5d73808afd943d
Prateek Soni <prateek_soni@outlook.com>
2022-09-09 01:23:58 +0530

    init express api
    Signed-off-by: Prateek Soni <prateek_soni@outlook.com>

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+export default app;
